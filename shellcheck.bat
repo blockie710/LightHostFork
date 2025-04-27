63163e0911c6b62c6f162fe577410984a7ebd422
blockie710 <bhogle01@gmail.com>
2025-04-27 17:01:54 -0400

    Refactor shellcheck.bat to simplify executable path handling and remove existence check

--- shellcheck.bat
+++ shellcheck.bat
@@ -1,10 +1,2 @@
 @echo off
-set "SHELLCHECK_PATH=%~dp0\tools\shellcheck\shellcheck.exe"
-
-if not exist "%SHELLCHECK_PATH%" (
-    echo ERROR: ShellCheck executable not found at %SHELLCHECK_PATH%
-    echo Please run Utilities\install_shellcheck.bat to install ShellCheck.
-    exit /b 1
-)
-
-"%SHELLCHECK_PATH%" %*
+"%~dp0\..\tools\shellcheck\shellcheck.exe" %*
